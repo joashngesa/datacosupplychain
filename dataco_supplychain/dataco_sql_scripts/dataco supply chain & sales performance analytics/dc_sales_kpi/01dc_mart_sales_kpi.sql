baf9468e8474223676810dbecdf3399d00a976f8
ovete28 <joashngesa13@gmail.com>
2026-03-27 11:12:45 -0600

    changed folders and file structure and naming

---
+++ dataco_supplychain/dataco_sql_scripts/dataco supply chain & sales performance analytics/dc_sales_kpi/01dc_mart_sales_kpi.sql
@@ -0,0 +1,11 @@
+-- SUBJECT: KPI ANALYSIS
+-- PURPOSE: BUSINESS PERFORMANCE METRICS
+-- GRAIN: ENTIRE DATASET(no grouping initially)
+
+--SUPPLYCHAIN QUESTIONS 
+--1. What is total profit?
+--2. What is total revenue?
+--3. What is the total number of orders?
+--4. What is the total quantity sold?
+--5. What is the average order value?
+--6. What is the total discount given?
